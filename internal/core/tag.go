ddc71e2df32f2fbab786240999f5f8843e61174d
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-05 17:56:16 +0000

    [new] tag endpoint

---
+++ internal/core/tag.go
@@ -0,0 +1,152 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/GOOFR-Group/store-back-end/internal/oapi"
+	"github.com/GOOFR-Group/store-back-end/internal/storage"
+	"github.com/gocraft/dbr/v2"
+	"github.com/google/uuid"
+)
+
+// PostTag creates a new tag
+func PostTag(req oapi.PostTagJSONRequestBody) error {
+	var id uuid.UUID
+	var err error
+
+	if id, err = uuid.NewRandom(); err != nil {
+		return fmt.Errorf(ErrGeneratingUUID, err.Error())
+	}
+
+	if err = handleTransaction(nil, func(tx dbr.SessionRunner) error {
+		return storage.CreateNewTag(tx, storage.Tag{
+			ID:   id,
+			Name: req.Name,
+		})
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetTag gets a tag
+func GetTag(params oapi.GetTagParams) ([]oapi.TagSchema, error) {
+	var objects []storage.Tag
+
+	if err := handleTransaction(nil, func(tx dbr.SessionRunner) error {
+		var err error
+
+		if params.Id == nil {
+			if objects, err = storage.GetAllTags(tx); err != nil {
+				return err
+			}
+		} else {
+			id, err := uuid.Parse(*params.Id)
+			if err != nil {
+				return err
+			}
+
+			var object storage.Tag
+			var ok bool
+			object, ok, err = storage.GetTagByID(tx, id)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				return ErrObjectNotFound
+			}
+
+			objects = append(objects, object)
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return getTagsFromModel(objects), nil
+}
+
+// PutTag updates a tag
+func PutTag(params oapi.PutTagParams, req oapi.PutTagJSONRequestBody) error {
+	if err := handleTransaction(nil, func(tx dbr.SessionRunner) error {
+		var err error
+		var ok bool
+
+		id, err := uuid.Parse(params.Id)
+		if err != nil {
+			return err
+		}
+
+		ok, err = storage.UpdateTagByID(tx, storage.Tag{
+			ID:   id,
+			Name: req.Name,
+		})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrObjectNotFound
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteTag deletes a tag
+func DeleteTag(params oapi.DeleteTagParams) (oapi.TagSchema, error) {
+	var object storage.Tag
+
+	if err := handleTransaction(nil, func(tx dbr.SessionRunner) error {
+		var err error
+		var ok bool
+
+		id, err := uuid.Parse(params.Id)
+		if err != nil {
+			return err
+		}
+
+		object, ok, err = storage.GetTagByID(tx, id)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrObjectNotFound
+		}
+
+		ok, err = storage.DeleteTagByID(tx, id)
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return ErrObjectNotFound
+		}
+
+		return nil
+	}); err != nil {
+		return oapi.TagSchema{}, err
+	}
+
+	return getTagFromModel(object), nil
+}
+
+func getTagFromModel(model storage.Tag) oapi.TagSchema {
+	id := model.ID.String()
+	return oapi.TagSchema{
+		Id:   &id,
+		Name: model.Name,
+	}
+}
+
+func getTagsFromModel(model []storage.Tag) []oapi.TagSchema {
+	array := make([]oapi.TagSchema, len(model))
+	for i, m := range model {
+		array[i] = getTagFromModel(m)
+	}
+	return array
+}
--- internal/handlers/tag.go
+++ internal/handlers/tag.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/GOOFR-Group/store-back-end/internal/core"
 	"github.com/GOOFR-Group/store-back-end/internal/oapi"
 )
 
@@ -10,16 +14,116 @@ const handlerTag = "tag"
 
 // PostTag handles the /tag Post endpoint
 func (*StoreImpl) PostTag(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrParsingRequest, err.Error())}
+		if err = writeResponse(w, response, http.StatusBadRequest, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	}
+
+	req := oapi.PostTagJSONRequestBody{}
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrParsingRequest, err.Error())}
+		if err = writeResponse(w, response, http.StatusBadRequest, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	}
+
+	err = core.PostTag(req)
+	switch err {
+	case nil:
+	default:
+		logInternalError(handlerTag, ErrInternalServer, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 // GetTag handles the /tag Get endpoint
 func (*StoreImpl) GetTag(w http.ResponseWriter, r *http.Request, params oapi.GetTagParams) {
+	response, err := core.GetTag(params)
+	switch err {
+	case nil:
+	case core.ErrObjectNotFound:
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrTagNotFound, *params.Id)}
+		if err = writeResponse(w, response, http.StatusNotFound, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	default:
+		logInternalError(handlerTag, ErrInternalServer, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = writeResponse(w, response, http.StatusOK, header{contentTypeHeader, contentTypeJSON}); err != nil {
+		logInternalError(handlerTag, ErrWritingResponse, err)
+	}
 }
 
 // PutTag handles the /tag Put endpoint
 func (*StoreImpl) PutTag(w http.ResponseWriter, r *http.Request, params oapi.PutTagParams) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrParsingRequest, err.Error())}
+		if err = writeResponse(w, response, http.StatusBadRequest, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	}
+
+	req := oapi.PutTagJSONRequestBody{}
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrParsingRequest, err.Error())}
+		if err = writeResponse(w, response, http.StatusBadRequest, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	}
+
+	err = core.PutTag(params, req)
+	switch err {
+	case nil:
+	case core.ErrObjectNotFound:
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrTagNotFound, params.Id)}
+		if err = writeResponse(w, response, http.StatusNotFound, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	default:
+		logInternalError(handlerTag, ErrInternalServer, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // DeleteTag handles the /tag Delete endpoint
 func (*StoreImpl) DeleteTag(w http.ResponseWriter, r *http.Request, params oapi.DeleteTagParams) {
+	response, err := core.DeleteTag(params)
+	switch err {
+	case nil:
+	case core.ErrObjectNotFound:
+		response := oapi.ErrorSchema{Error: fmt.Sprintf(ErrTagNotFound, params.Id)}
+		if err = writeResponse(w, response, http.StatusNotFound, header{contentTypeHeader, contentTypeJSON}); err != nil {
+			logInternalError(handlerTag, ErrWritingResponse, err)
+		}
+		return
+	default:
+		logInternalError(handlerTag, ErrInternalServer, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = writeResponse(w, response, http.StatusOK, header{contentTypeHeader, contentTypeJSON}); err != nil {
+		logInternalError(handlerTag, ErrWritingResponse, err)
+	}
 }
