4375f6594d549ac34a51097865c1929ccce53f98
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-05 18:28:52 +0000

    [new] newsletter storage

--- internal/storage/newsletter.go
+++ internal/storage/newsletter.go
@@ -1,12 +1,57 @@
 package storage
 
 import (
-	"github.com/google/uuid"
+	"github.com/gocraft/dbr/v2"
 )
 
 const NewsletterTable = "newsletter"
 
 // tgcon - used to generate constants for each field's tag
 type Newsletter struct {
-	Email uuid.UUID `db:"email"`
+	Email string `db:"email"`
+}
+
+func CreateNewsletter(t Transaction, model Newsletter) error {
+	_, err := t.InsertInto(NewsletterTable).
+		Columns(NewsletterEmailDb).
+		Record(model).
+		Exec()
+
+	return err
+}
+
+func ReadAllNewsletters(t Transaction) (objects []Newsletter, err error) {
+	_, err = t.Select("*").
+		From(NewsletterTable).
+		Load(&objects)
+
+	return
+}
+
+func ReadNewsletterByID(t Transaction, email string) (object Newsletter, ok bool, err error) {
+	err = t.Select("*").
+		From(NewsletterTable).
+		Where(NewsletterEmailDb+" = ?", email).
+		LoadOne(&object)
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
+}
+func DeleteNewsletterByID(t Transaction, email string) (ok bool, err error) {
+	_, err = t.DeleteFrom(NewsletterTable).
+		Where(NewsletterEmailDb+" = ?", email).
+		Exec()
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
 }
