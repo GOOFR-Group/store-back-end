d0abe5bc727e5cf87b0775171170fced46e2847c
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-06 19:43:51 +0000

    [new] tag_game storage

--- internal/storage/tag_game.go
+++ internal/storage/tag_game.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"github.com/gocraft/dbr/v2"
 	"github.com/google/uuid"
 )
 
@@ -10,4 +11,44 @@ const TagGameTable = "tag_game"
 type TagGame struct {
 	IDTag  uuid.UUID `db:"id_tag"`
 	IDGame uuid.UUID `db:"id_game"`
+}
+
+func CreateTagGame(t Transaction, tagID, gameID uuid.UUID) error {
+	_, err := t.InsertInto(TagGameTable).
+		Pair(TagGameIDTagDb, tagID).
+		Pair(TagGameIDGameDb, gameID).
+		Exec()
+
+	return err
+}
+
+func ReadTagGameByID(t Transaction, tagID, gameID uuid.UUID) (object TagGame, ok bool, err error) {
+	err = t.Select("*").
+		From(TagGameTable).
+		Where(TagGameIDTagDb+" = ?", tagID).
+		Where(TagGameIDGameDb+" = ?", gameID).
+		LoadOne(&object)
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
+}
+
+func DeleteTagGameByID(t Transaction, tagID, gameID uuid.UUID) (ok bool, err error) {
+	_, err = t.DeleteFrom(TagGameTable).
+		Where(TagGameIDTagDb+" = ?", tagID).
+		Where(TagGameIDGameDb+" = ?", gameID).
+		Exec()
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
 }
