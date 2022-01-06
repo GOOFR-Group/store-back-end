867abbf2ffe93386b70dc97473d2278602d8cee6
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-06 19:43:31 +0000

    [new] image_game storage

--- internal/storage/image_game.go
+++ internal/storage/image_game.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"github.com/gocraft/dbr/v2"
 	"github.com/google/uuid"
 )
 
@@ -11,4 +12,51 @@ type ImageGame struct {
 	ID     uuid.UUID `db:"id"`
 	IDGame uuid.UUID `db:"id_game"`
 	Image  string    `db:"image"`
+}
+
+func CreateImageGame(t Transaction, model ImageGame) error {
+	_, err := t.InsertInto(ImageGameTable).
+		Columns(ImageGameIDDb, ImageGameIDGameDb, ImageGameImageDb).
+		Record(model).
+		Exec()
+
+	return err
+}
+
+func ReadImageGamesByGameID(t Transaction, id uuid.UUID) (objects []ImageGame, err error) {
+	_, err = t.Select("*").
+		From(ImageGameTable).
+		Where(ImageGameIDGameDb+" = ?", id).
+		Load(&objects)
+
+	return
+}
+
+func ReadImageGameByID(t Transaction, id uuid.UUID) (object ImageGame, ok bool, err error) {
+	err = t.Select("*").
+		From(ImageGameTable).
+		Where(ImageGameIDDb+" = ?", id).
+		LoadOne(&object)
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
+}
+
+func DeleteImageGameByID(t Transaction, id uuid.UUID) (ok bool, err error) {
+	_, err = t.DeleteFrom(ImageGameTable).
+		Where(ImageGameIDDb+" = ?", id).
+		Exec()
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
 }
