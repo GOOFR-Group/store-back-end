c9ed37d758282b785caf05a2c8946d50de99b25a
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-08 02:48:04 +0000

    [new] access storage

--- internal/storage/access.go
+++ internal/storage/access.go
@@ -2,8 +2,8 @@ package storage
 
 import (
 	"database/sql"
-	"time"
 
+	"github.com/gocraft/dbr/v2"
 	"github.com/google/uuid"
 )
 
@@ -11,10 +11,61 @@ const AccessTable = "access"
 
 // tgcon - used to generate constants for each field's tag
 type Access struct {
-	ID        uuid.UUID      `db:"id"`
-	IDClient  uuid.UUID      `db:"id_client"`
-	OAuth     bool           `db:"oauth"`
-	Email     string         `db:"email"`
-	Password  sql.NullString `db:"password"`
-	CreatedAt time.Time      `db:"created_at"`
+	ID       uuid.UUID      `db:"id"`
+	IDClient uuid.UUID      `db:"id_client"`
+	OAuth    bool           `db:"oauth"`
+	Email    string         `db:"email"`
+	Password sql.NullString `db:"password"`
+}
+
+func CreateAccess(t Transaction, model Access) error {
+	_, err := t.InsertInto(AccessTable).
+		Columns(AccessIDDb, AccessIDClientDb, AccessOAuthDb, AccessEmailDb, AccessPasswordDb).
+		Record(model).
+		Exec()
+
+	return err
+}
+
+func ReadAccessByClientID(t Transaction, id uuid.UUID) (object Access, ok bool, err error) {
+	err = t.Select("*").
+		From(AccessTable).
+		Where(AccessIDClientDb+" = ?", id).
+		LoadOne(&object)
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
+}
+
+func ReadAccessByEmail(t Transaction, email string) (object Access, ok bool, err error) {
+	err = t.Select("*").
+		From(AccessTable).
+		Where(AccessEmailDb+" = ?", email).
+		LoadOne(&object)
+
+	switch err {
+	case nil:
+		ok = true
+	case dbr.ErrNotFound:
+		err = nil
+	}
+	return
+}
+
+func UpdateAccessByClientID(t Transaction, model Access) error {
+	_, err := t.Update(AccessTable).
+		SetMap(map[string]interface{}{
+			AccessOAuthDb:    model.OAuth,
+			AccessEmailDb:    model.Email,
+			AccessPasswordDb: model.Password,
+		}).
+		Where(AccessIDClientDb+" = ?", model.IDClient).
+		Exec()
+
+	return err
 }
